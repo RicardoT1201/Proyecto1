ce7540dd99dcdb3806175480f6aaca2072fa6644
RickyT <tortoledoricardo@gmail.com>
2024-03-15 20:20:49 +0100

    a

--- app/src/main/java/com/example/proyecto1/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.proyecto1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto.R
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate (savedInstanceState: Bundle?) {
